fa37c2a75fa155ef91d1ee7f910741435e12ca3c
Petr Janik <peta.janik@email.cz>
2023-02-11 13:32:31 +0100

    Reformat

--- Sklenda.py
+++ Sklenda.py
@@ -1,13 +1,14 @@
 # Sklenda
-# Originally by Petr Janik
+# Developed by Petr Janik
 # Idea by Zdenek Sklenar
 
-import pygame
 import sys
+
+import pygame
 from pygame.locals import *
 
-BOARD_WIDTH = 4  
-BOARD_HEIGHT = 4  
+BOARD_WIDTH = 4
+BOARD_HEIGHT = 4
 TILE_SIZE = 80
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 500
@@ -31,14 +32,13 @@ CIRCLE = 'circle'
 SQUARE = 'square'
 SMALL = 'small'
 BIG = 'big'
-FALSE= False
+FALSE = False
 TRUE = True
 
 FPS_CLOCK = None
 DISPLAY_SURFACE = None
 BASIC_FONT = None
 BUTTONS = None
-
 
 
 def main():
@@ -53,10 +53,12 @@ def main():
     while True:
         run_game()
 
+
 def terminate():
     pygame.quit()
     sys.exit()
-   
+
+
 def run_game():
     icons = [
         [
@@ -64,59 +66,59 @@ def run_game():
             [None, None, None, None],
             [None, None, None, None],
             [None, None, None, None],
-            ],
+        ],
         [
             [None, None, None, None],
             [None, None, None, None],
             [None, None, None, None],
             [None, None, None, None],
-            ],
+        ],
         [
             [None, None, None, None],
             [None, None, None, None],
             [None, None, None, None],
             [None, None, None, None],
-            ],
+        ],
         [
             [None, None, None, None],
             [None, None, None, None],
             [None, None, None, None],
             [None, None, None, None],
-           ]
-            ]
+        ]
+    ]
     icons2 = [
         [
             [CIRCLE, YELLOW, FALSE, SMALL],
             [CIRCLE, YELLOW, FALSE, BIG],
             [CIRCLE, YELLOW, TRUE, SMALL],
             [CIRCLE, YELLOW, TRUE, BIG],
-            ],
+        ],
         [
             [CIRCLE, BROWN, FALSE, SMALL],
             [CIRCLE, BROWN, FALSE, BIG],
             [CIRCLE, BROWN, TRUE, SMALL],
             [CIRCLE, BROWN, TRUE, BIG],
-            ],
+        ],
         [
             [SQUARE, YELLOW, FALSE, SMALL],
             [SQUARE, YELLOW, FALSE, BIG],
             [SQUARE, YELLOW, TRUE, SMALL],
             [SQUARE, YELLOW, TRUE, BIG],
-            ],
+        ],
         [
             [SQUARE, BROWN, FALSE, SMALL],
             [SQUARE, BROWN, FALSE, BIG],
             [SQUARE, BROWN, TRUE, SMALL],
             [SQUARE, BROWN, TRUE, BIG],
-           ]
-            ]    
+        ]
+    ]
     WIN = False
     choosed = False
     placed = 0
     shape, color, point, size = None, None, None, None
     game_state = 1
     new_surf, new_rect = make_text('New Game', BLACK, TILE_COLOR, WINDOW_WIDTH - 145, WINDOW_HEIGHT - 50)
-    while True:    
+    while True:
         if game_state == 1:
             message = "Player 1 chooses block."
         elif game_state == 2:
@@ -130,7 +132,7 @@ def run_game():
 
         if placed == 16:
             message = "It is a tie! Press New Game to play again."
-            
+
         if WIN:
             message = "Player " + player + " wins!"
             for event in pygame.event.get():
@@ -139,24 +141,25 @@ def run_game():
                 elif event.type == pygame.MOUSEBUTTONUP:
                     if new_rect.collidepoint(event.pos):
                         return
-                
+
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                 terminate()
             elif event.type == pygame.MOUSEBUTTONUP:
-                
+
                 if game_state == 1 or game_state == 3:
                     if get_tile_clicked2(event.pos[0], event.pos[1]) != (None, None):
                         tile_x, tile_y = get_tile_clicked2(event.pos[0], event.pos[1])
                         if icons2[tile_x][tile_y][0] != None:
-                            shape, color, point, size = icons2[tile_x][tile_y][0], icons2[tile_x][tile_y][1], icons2[tile_x][tile_y][2], icons2[tile_x][tile_y][3]
+                            shape, color, point, size = icons2[tile_x][tile_y][0], icons2[tile_x][tile_y][1], \
+                            icons2[tile_x][tile_y][2], icons2[tile_x][tile_y][3]
                             icons2[tile_x][tile_y] = [None, None, None, None]
                             choosed = True
                             if game_state == 1:
                                 game_state = 2
                             elif game_state == 3:
                                 game_state = 4
-                            
+
                 elif game_state == 2 or game_state == 4:
                     if get_tile_clicked(event.pos[0], event.pos[1]) != (None, None):
                         tile_x, tile_y = get_tile_clicked(event.pos[0], event.pos[1])
@@ -169,34 +172,39 @@ def run_game():
                                 game_state = 3
                             elif game_state == 4:
                                 game_state = 1
-    
+
                 if new_rect.collidepoint(event.pos):
                     return
-                
+
         draw_board(icons, icons2, shape, color, point, size, choosed, new_surf, new_rect, message)
 
         pygame.display.update()
         FPS_CLOCK.tick(FPS)
+
 
 def get_left_top_of_tile(tile_x, tile_y):
     left = (tile_x * TILE_SIZE) + 40
     top = (tile_y * TILE_SIZE) + 110
     return (left, top)
 
+
 def get_left_top_of_tile2(tile_x, tile_y):
     left = (tile_x * TILE_SIZE) + 440
     top = (tile_y * TILE_SIZE) + 110
     return (left, top)
 
+
 def draw_tile(tile_x, tile_y):
     left, top = get_left_top_of_tile(tile_x, tile_y)
     pygame.draw.rect(DISPLAY_SURFACE, TILE_COLOR, (left, top, TILE_SIZE, TILE_SIZE))
-    pygame.draw.rect(DISPLAY_SURFACE, BORDER_COLOR, (left-1, top-1, TILE_SIZE+2, TILE_SIZE+2), 1)
+    pygame.draw.rect(DISPLAY_SURFACE, BORDER_COLOR, (left - 1, top - 1, TILE_SIZE + 2, TILE_SIZE + 2), 1)
+
 
 def draw_tile2(tile_x, tile_y):
     left, top = get_left_top_of_tile2(tile_x, tile_y)
     pygame.draw.rect(DISPLAY_SURFACE, TILE_COLOR, (left, top, TILE_SIZE, TILE_SIZE))
-    pygame.draw.rect(DISPLAY_SURFACE, BORDER_COLOR, (left-1, top-1, TILE_SIZE+2, TILE_SIZE+2), 1)
+    pygame.draw.rect(DISPLAY_SURFACE, BORDER_COLOR, (left - 1, top - 1, TILE_SIZE + 2, TILE_SIZE + 2), 1)
+
 
 def make_text(text, color, bg_color, top, left):
     text_surf = BASIC_FONT.render(text, True, color, bg_color)
@@ -204,31 +212,33 @@ def make_text(text, color, bg_color, top, left):
     text_rect.topleft = (top, left)
     return (text_surf, text_rect)
 
+
 def draw_board(icons, icons2, shape, color, point, size, choosed, new_surf, new_rect, message=""):
     DISPLAY_SURFACE.fill(BG_COLOR)
     text_surf, text_rect = make_text(message, TEXT_COLOR, BG_COLOR, 40, 40)
     DISPLAY_SURFACE.blit(text_surf, text_rect)
 
-    if choosed:            
+    if choosed:
         display_choosed(icons2, shape, color, point, size)
-        
-    for tile_x in range(BOARD_WIDTH):
-        for tile_y in range(BOARD_WIDTH):
-                draw_tile(tile_x, tile_y)
-                
-    for tile_x in range(BOARD_WIDTH):
-        for tile_y in range(BOARD_WIDTH):
-                draw_tile2(tile_x, tile_y)
 
     for tile_x in range(BOARD_WIDTH):
         for tile_y in range(BOARD_WIDTH):
-                draw_icon(icons, tile_x, tile_y)
+            draw_tile(tile_x, tile_y)
 
     for tile_x in range(BOARD_WIDTH):
         for tile_y in range(BOARD_WIDTH):
-                draw_icon2(icons2, tile_x, tile_y)
+            draw_tile2(tile_x, tile_y)
+
+    for tile_x in range(BOARD_WIDTH):
+        for tile_y in range(BOARD_WIDTH):
+            draw_icon(icons, tile_x, tile_y)
+
+    for tile_x in range(BOARD_WIDTH):
+        for tile_y in range(BOARD_WIDTH):
+            draw_icon2(icons2, tile_x, tile_y)
 
     DISPLAY_SURFACE.blit(new_surf, new_rect)
+
 
 def display_choosed(list, shape, color, point, size):
     left, top = 280, 20
@@ -250,8 +260,10 @@ def display_choosed(list, shape, color, point, size):
         if point:
             pygame.draw.circle(DISPLAY_SURFACE, POINT_COLOR, center_point, 10)
 
+
 def draw_icon(list, tile_x, tile_y):
-    shape, color, point, size = list[tile_x][tile_y][0], list[tile_x][tile_y][1], list[tile_x][tile_y][2], list[tile_x][tile_y][3]
+    shape, color, point, size = list[tile_x][tile_y][0], list[tile_x][tile_y][1], list[tile_x][tile_y][2], \
+    list[tile_x][tile_y][3]
     left, top = get_left_top_of_tile(tile_x, tile_y)
     if shape == CIRCLE:
         center_point = (left + 40, top + 40)
@@ -270,9 +282,11 @@ def draw_icon(list, tile_x, tile_y):
         pygame.draw.rect(DISPLAY_SURFACE, color, rect)
         if point:
             pygame.draw.circle(DISPLAY_SURFACE, POINT_COLOR, center_point, 10)
-            
+
+
 def draw_icon2(list, tile_x, tile_y):
-    shape, color, point, size = list[tile_x][tile_y][0], list[tile_x][tile_y][1], list[tile_x][tile_y][2], list[tile_x][tile_y][3]
+    shape, color, point, size = list[tile_x][tile_y][0], list[tile_x][tile_y][1], list[tile_x][tile_y][2], \
+    list[tile_x][tile_y][3]
     left, top = get_left_top_of_tile2(tile_x, tile_y)
     if shape == CIRCLE:
         center_point = (left + 40, top + 40)
@@ -292,6 +306,7 @@ def draw_icon2(list, tile_x, tile_y):
         if point:
             pygame.draw.circle(DISPLAY_SURFACE, POINT_COLOR, center_point, 10)
 
+
 def get_tile_clicked(x, y):
     for tile_x in range(BOARD_WIDTH):
         for tile_y in range(BOARD_HEIGHT):
@@ -300,6 +315,7 @@ def get_tile_clicked(x, y):
             if tile_rect.collidepoint(x, y):
                 return (tile_x, tile_y)
     return (None, None)
+
 
 def get_tile_clicked2(x, y):
     for tile_x in range(BOARD_WIDTH):
@@ -310,6 +326,7 @@ def get_tile_clicked2(x, y):
                 return (tile_x, tile_y)
     return (None, None)
 
+
 def check_for_quit(icons, tile_x, tile_y):
     list = [None, None, None, None]
     lp_icon = icons[tile_x][tile_y]
@@ -317,12 +334,12 @@ def check_for_quit(icons, tile_x, tile_y):
     top_right = True
     bottom_left = True
     bottom_right = True
-    for i in range(4): #checks horizontal line
+    for i in range(4):  # checks horizontal line
         icon = icons[i][tile_y]
         list[i] = check_if_same(icons, lp_icon, icon, tile_x, tile_y)
     if compare(list):
         return True
-    for i in range(4): #checks vertical line
+    for i in range(4):  # checks vertical line
         icon = icons[tile_x][i]
         list[i] = check_if_same(icons, lp_icon, icon, tile_x, tile_y)
     if compare(list):
@@ -385,6 +402,7 @@ def check_for_quit(icons, tile_x, tile_y):
         return True
     return False
 
+
 def check_if_same(icons, lp_icon, icon, tile_x, tile_y):
     values = []
     for i in range(4):
@@ -394,6 +412,7 @@ def check_if_same(icons, lp_icon, icon, tile_x, tile_y):
             values.append(0)
     return (values)
 
+
 def compare(list):
     for i in range(4):
         count = 0
@@ -401,7 +420,8 @@ def compare(list):
             count += list[j][i]
             if count == 4:
                 return True
-    return False    
-        
+    return False
+
+
 if __name__ == '__main__':
     main()
